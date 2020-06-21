bb2b2963be47408d947114eb1a664c36ae36e2b4
Zubeyde <zubeydeserap1221@gmail.com>
2020-06-21 21:33:25 +0200

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -7,6 +7,8 @@ public class FirstClass {
 		System.out.println("Ilk commit from local repository");
 		System.out.println("Ikinci commit from local repository");
 		
+		System.out.println("Yeni branch olusturdum ve adi feature");
+		
 	}
 
 }
