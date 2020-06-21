b27d9ade39277fcf71a810bc4fa7127206bbe155
Zubeyde <zubeydeserap1221@gmail.com>
2020-06-21 21:14:37 +0200

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Ilk commit from local repository");
+		System.out.println("Ikinci commit from local repository");
 		
 	}
 
